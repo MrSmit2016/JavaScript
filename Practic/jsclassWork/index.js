d0020b56d0c6fc972767021e66f6d1636935ab5d
MishaDanish <werete095@gmail.com>
2019-11-10 14:03:28 +0200

    ok

---
+++ Practic/jsclassWork/index.js
@@ -0,0 +1,127 @@
+"use strict"
+
+console.log("hello world");
+
+// const namber = 7;
+
+// number = 5;
+
+// let number = 6;
+// let string = "string";
+// let boolean = true; // false
+
+
+
+// let a = 2;
+// let b = 3;
+
+// let result = a+b;
+
+// console.log("Our result", result);
+
+// let apple;
+
+// console.log(apple);
+
+// var a = "hello"
+// console.log(typeof a);
+
+// var c = confirm("Day Daneg");
+// console.log(c);
+
+// const z = prompt("Enter your name")
+
+// console.log(z)
+
+
+
+// let a = 7;
+
+// let b = "9 nokk";
+
+// let result = a % b;
+
+// console.log(result);
+
+// console.log(a === Number(b));
+
+
+// let a = "25px"
+// console.log(Number.parseInt(a) + 50 +"px");
+
+
+
+// let a = "asdfg";
+// console.log(Number.parseFloat(a) + 50);
+
+// console.log(Number.isNaN(+a));
+
+
+
+// console.log(0.1+0.3 === 0.4)
+// let a = 0.1+0.2;
+//  console.log(a.toFixed(2))
+
+
+
+
+
+// console.log((Math.random()*9).toFixed(0),(Math.random()*9).toFixed(0),(Math.random()*9).toFixed(0));
+
+
+
+// const a = 1, b = 5, c = 8;
+// console.log(Math.max(a,b,c))
+
+
+
+// console.log(Math.ceil(0.5))
+// console.log(Math.floor(0.5)) 
+// console.log(Math.round(0.5))
+// console.log(Math.pow(5, 7)) є
+
+
+
+// const age = 25;
+// console.log("Hello I'm" + age + "years old");
+
+// const name = "Rodion"
+
+// console.log(name.length);
+// console.log(name.indexOf("o"));
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+// console.log(name.includes("n"));
+
+
+
+
+// const firstName = "Rodion";
+// const surName = "Romanov";
+// const age = 25;
+// console.log(`Hello my name is ${firstName} ${surName} and I'm ${age} years old`);
+
+
+
+// alert(true)
+// console.log(String(true) === "true");
+
+
+
+
+
+
+// const name = "Генератор защитного поля";
+// let price = 1000;
+// price = 2000;
+// console.log(`Вибран <<${name}>> , цена за штуку ${price} кредитов`);
+
+const day = 17;
+const mount = 10;
+const year = 2048;
+
+const date = `${day} + "/" + ${month} + "/" + ${year}`;
+const message = `Доброе утр, сегодня ${date}, за бортом отличная погода!`; 
+console.log(date);
+console.log(message);
+
